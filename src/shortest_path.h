8f2507704015ff3ef18e756e73cc23330bf7c668
Trevor Harmon <tharmon@laspositascollege.edu>
2023-12-06 17:53:05 -0800

    Add solution (part 1)

--- src/shortest_path.h
+++ src/shortest_path.h
@@ -2,6 +2,8 @@
 
 #include "graph.h"
 
+#include <limits>
+#include <queue>
 #include <vector>
 
 template <typename VertexElement, typename EdgeDistance>
@@ -19,6 +21,8 @@ private:
     typedef typename G::EdgeItor EdgeItor;
     
     G graph;
+
+    const EdgeDistance INFINITE_DISTANCE = std::numeric_limits<EdgeDistance>::max();
 
 public:
 
@@ -40,12 +44,48 @@ public:
     }
 
     std::vector<std::pair<VertexElement,EdgeDistance>> findShortestPath(Vertex& source, Vertex& target) {
+        VertexList verts = graph.vertices();
+        for (VertexItor pv = verts.begin(); pv != verts.end(); ++pv) {
+            (**pv).shortestDistance = INFINITE_DISTANCE;
+        }
 
-        // TODO
+        (*source).shortestDistance = 0;
+
+        auto cmp = [](Vertex* left, Vertex* right) { return (**left).shortestDistance > (**right).shortestDistance; };
+        std::priority_queue<Vertex*, std::vector<Vertex*>, decltype(cmp)> Q(cmp);
+        verts = graph.vertices();
+        for (VertexItor pv = verts.begin(); pv != verts.end(); ++pv) {
+            Vertex& v = *pv;
+            Q.push(&v);
+        }
+
+        while (!Q.empty()) {
+            Vertex* u = Q.top();
+            Q.pop();
+    
+            if ((**u).shortestDistance == INFINITE_DISTANCE) {
+                continue;
+            }
+
+            EdgeList edges = u->incidentEdges();
+            for (EdgeItor pe = edges.begin(); pe != edges.end(); ++pe) {
+                Edge edge = (*pe);
+                Vertex& z = edge.opposite(*u);
+                EdgeDistance r = (**u).shortestDistance + (*edge);
+                if (r < (*z).shortestDistance) {
+                    (*z).shortestDistance = r;
+                    Q.push(&z);
+                }
+            }
+        }
 
         std::vector<std::pair<VertexElement,EdgeDistance>> vertices;
-
-        // TODO
+        verts = graph.vertices();
+        for (VertexItor pv = verts.begin(); pv != verts.end(); ++pv) {
+            if (*pv == target) {
+                vertices.push_back(std::make_pair((**pv).element, (**pv).shortestDistance));
+            }
+        }
 
         return vertices;
     }
